375bfbbf43a092c8d1921f0637f88e228d9fab0a
Gold-Sea <1800013512@pku.edu.cn>
2021-11-22 20:25:09 +0800

    fix bugs and add inference

--- genetic.py
+++ genetic.py
@@ -14,19 +14,49 @@ class Chromosome:
 
         self.Fitness = fitness
         num_tone = jsonstr['num_tones']
-        self.length = num_tone + 2
-        self.geneset = [i for i in range(self.length)]
+        self.length = jsonstr['bars'] * jsonstr['pluses']
+        self.num_tone = num_tone + 2
+        self.geneset = [i for i in range(self.num_tone)]
         self.genes = None
     
     # return a numpy array with shape(num_tones + 2,)
     def generate_parent(self): 
-        return np.random.randint(low=0, high=self.length,size=(self.length,))
+        return np.random.randint(low=0, high=self.num_tone,size=(self.length,))
 
     def mutate(self, typeid):
-        pass
+        if typeid == 0:
+            randindex = np.random.randint(low=0, high=self.length)
+            if_pluse = np.random.randint(low=0, high=2)
+            original_val = self.genes[randindex]
+            # Upward overflow
+            if original_val + 8 >= self.num_tone:
+                if_pluse = 0
+            if original_val - 8 < 0:
+                if_pluse = 1
+            if if_pluse:
+                self.genes[randindex] += 8
+            else:
+                self.genes[randindex] -= 8
+
+        elif typeid == 1:
+            randindex = np.random.randint(low=0, high=self.length)
+            original_val = self.genes[randindex]
+            randval = np.random.randint(low=0, high=self.length)
+            while(randval != original_val):
+                randval = np.random.randint(low=0, high=self.length)
+            self.genes[randindex] = randval
+
+        elif typeid == 2:
+            randindex = np.random.randint(low=0, high=self.length - 1)
+            temp = self.genes[randindex]
+            self.genes[randindex] = self.genes[randindex + 1]
+            self.genes[randindex + 1] = temp
+        
+        else:
+            raise ValueError('Type ID is one of 0,1,2')
 
     
         
 
 if __name__ == "__main__":
-    print(np.random.randint(low=0, high=5,size=(5,)))
+    
--- model.py
+++ model.py
@@ -1,8 +1,17 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import json
+
+from torch.storage import T
+
+with open("./GA.json",'r') as load_f:
+    load_dict = json.load(load_f)
+    path = load_dict['model_path']
+    bs = load_dict["batch_size"]
+    is_trained = load_dict["is_trained"]
 
 
 class Net(nn.Module):
@@ -21,28 +30,47 @@ class Net(nn.Module):
         return out
 
 
-
+# train and save the model checkpoint
 def train(model, features, labels, epochs, path):
     assert len(features) == len(labels)
     optimizer = torch.optim.SGD(model.parameters(),lr = 0.05)
     loss_func = torch.nn.MSELoss()
-    for i in epochs:
-        for i in range(len(features)):
-            input = features[i]
-            label = labels[i]
-            prediction = model(input)
-            loss = loss_func(prediction, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    for j in range(epochs):
+        print("Training in %s epochs"%{j})
+        prediction = model(features)
+        loss = loss_func(prediction, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
     torch.save(model, path) 
 
+# model inference
+def inference(genes):
+    assert isinstance(genes, np.ndarray)
+    input = torch.tensor(genes,dtype=torch.float32)
+    mod = torch.load(path)
+    return mod(input).detach().numpy()
 
 
 if __name__ == "__main__":
-    net = Net(32,64,1)
+    x = torch.unsqueeze(torch.linspace(-1,1,100),dim=1)
+    y = x * 2 + 1
+    print(x.shape)
+    net = Net(1,16,1)
     print(net.children)
-    with open("./GA.json",'r') as load_f:
-        load_dict = json.load(load_f)
-        path = load_dict['model_path']
-    train(net, )
+    if not is_trained:
+        train(net, x, y ,500, path)
+
+    # test model inference
+    # x = []
+    # for i in range(100):
+    #     x.append(i/100)
+    # x = np.array(x)
+    # x = np.reshape(x, (100,1))
+    # print(x)
+    # print(inference(x))
+
+    # with open("./GA.json",'w') as f:
+    #     load_dict["is_trained"] = True
+    #     json.dump(load_dict, f)
+    
